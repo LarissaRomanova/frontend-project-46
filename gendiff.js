c58590f572622a9965e4f69ee91d1f2b4e260145
Larisa Romanova <larissa.kstu@gmail.com>
2025-07-04 19:05:36 +0300

    Create reading and parsing file. 3 step of project

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import { readFile } from './parsers.js';
 
 program
   .name('gendiff')
@@ -8,6 +9,17 @@ program
   .version('1.0.0')
   .option('-f, --format [type]', 'output format')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    try {
+      const data1 = readFile(filepath1);
+      const data2 = readFile(filepath2);
+
+      console.log('Data from first file:', data1);
+      console.log('Data from second file:', data2);
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+    }
+  });
 
 program.parse();
---
+++ parsers.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+
+export function readFile(filePath) {
+  const absolutePath = path.resolve(process.cwd(), filePath);
+  const data = fs.readFileSync(absolutePath, 'utf-8');
+
+  const extension = filePath.slice(filePath.lastIndexOf('.') + 1);
+
+  if (extension === 'json') {
+    return JSON.parse(data);
+  }
+
+  throw new Error(`Unsupported file extension: ${extension}`);
+}
